03fac7852aab44c80f4899bae64ae4d67e41dfdd
Henrique Coelho <henriquecoelhomv@gmail.com>
2021-08-07 18:13:39 -0300

    Primeiro Commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function menuToggle() {
+    let menu = document.getElementById("menuarea");
+    if(menu.style.width == '200px') {
+        menu.style.width = '0px';
+    } else {
+        menu.style.width = '200px';
+
+    }
+    
+}
